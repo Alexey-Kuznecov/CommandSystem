bdc2edb95b4d846da8376a002a71249d4cc4f346
lexa1th@gmail.com <user03@DESKTOP-P3B43A9>
2025-09-10 08:12:43 +0800

    feat(mvvm): добавлены базовые классы ObservableObject и RelayCommand

---
+++ CommandSystem.Gui/MVVM/ObservableObject.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandSystem.Gui.MVVM
+{
+    /// <summary>
+    /// Базовый класс для ViewModel с поддержкой INotifyPropertyChanged
+    /// </summary>
+    public abstract class ObservableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        /// <summary>
+        /// Универсальный сеттер для свойств
+        /// </summary>
+        protected bool SetProperty<T>(ref T storage, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (Equals(storage, value))
+                return false;
+
+            storage = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+    }
+}
