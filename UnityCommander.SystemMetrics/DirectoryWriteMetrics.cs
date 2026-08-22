505e7364eafbde29445efb0c21d81034672fefb2
lexa1th <lexa1th@gmail.com>
2026-08-23 04:51:29 +0800

    Add runtime logging control

--- UnityCommander.SystemMetrics/DirectoryWriteMetrics.cs
+++ UnityCommander.SystemMetrics/DirectoryWriteMetrics.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading;
-using System.Timers;
+﻿
 using Timer = System.Timers.Timer;
 
 namespace UnityCommander.SystemMetrics
